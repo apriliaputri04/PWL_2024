c00b299c95ae0c68f361ad3023cbed839ad3857b
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-08 01:12:20 +0700

    P2 L9

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -60,3 +62,5 @@ Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class);
