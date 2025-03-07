ebbf77136e80ab58d665c599679bd95cf4c66b35
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-08 01:43:39 +0700

    P3 L2

--- routes/web.php
+++ routes/web.php
@@ -72,3 +72,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
 'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () { 
+    return view('hello', ['name' => 'Aprilia Putri Anggraeni']); 
+});
